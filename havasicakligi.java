398b9f96746067df953ea5621dd4826ae71c693c
Theo <alpotheo@gmail.com>
2023-06-08 03:39:29 +0300

    havasicakligi

---
+++ havasicakligi.java
@@ -0,0 +1,27 @@
+package Patika_Codes;
+
+import java.util.Scanner;
+
+public class havasicakligi {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık giriniz: ");
+        heat = input.nextInt();
+
+        if (heat<5){
+            System.out.println("Kayak yapabilirsiniz.");
+        }else if (heat<=25){
+            
+            if(heat<=15){
+                System.out.println("Sinemaya gidebilirsiniz.");
+            }
+            if (heat>=10){
+                System.out.println("Pikniğe gidebilirsiniz.");
+            }
+        }else {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
