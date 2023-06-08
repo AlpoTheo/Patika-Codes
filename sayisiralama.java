e99c076a75a146d67432331f5bff2992e208593c
Theo <alpotheo@gmail.com>
2023-06-08 03:41:21 +0300

    sayisiralama

---
+++ sayisiralama.java
@@ -0,0 +1,42 @@
+package Patika_Codes;
+
+import java.util.Scanner;
+
+public class sayisiralama {
+    
+    public static void main(String[] args) {
+        int sayi1, sayi2, sayi3;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Üç Sayıyı Küçükten Büyüğe Sıralama");
+
+        System.out.print("1. Sayıyı Giriniz: ");
+        sayi1 = input.nextInt();
+
+        System.out.print("2. Sayıyı Giriniz: ");
+        sayi2 = input.nextInt();
+
+        System.out.print("3. Sayıyı Giriniz: ");
+        sayi3 = input.nextInt();
+
+        if (sayi1 <= sayi2 && sayi1 <= sayi3) {
+            if (sayi2 <= sayi3) {
+                System.out.println("Sıralama: " + sayi1 + " <= " + sayi2 + " <= " + sayi3);
+            } else {
+                System.out.println("Sıralama: " + sayi1 + " <= " + sayi3 + " <= " + sayi2);
+            }
+        } else if (sayi2 <= sayi1 && sayi2 <= sayi3) {
+            if (sayi1 <= sayi3) {
+                System.out.println("Sıralama: " + sayi2 + " <= " + sayi1 + " <= " + sayi3);
+            } else {
+                System.out.println("Sıralama: " + sayi2 + " <= " + sayi3 + " <= " + sayi1);
+            }
+        } else {
+            if (sayi1 <= sayi2) {
+                System.out.println("Sıralama: " + sayi3 + " <= " + sayi1 + " <= " + sayi2);
+            } else {
+                System.out.println("Sıralama: " + sayi3 + " <= " + sayi2 + " <= " + sayi1);
+            }
+        }
+    }
+}
