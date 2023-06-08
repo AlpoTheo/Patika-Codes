ed3de2e0e2feed5352754154963d1c05a8bf415a
Theo <alpotheo@gmail.com>
2023-06-08 04:16:14 +0300

    ciftsayilar

---
+++ ciftsayilar.java
@@ -0,0 +1,23 @@
+package Patika_Codes;
+
+import java.util.Scanner;
+
+public class ciftsayilar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int number;
+
+        do {
+            System.out.print("Bir sayı giriniz (Çıkmak için tek bir sayı giriniz): ");
+            number = input.nextInt();
+
+            if (number % 2 == 0 && number % 4 == 0) {
+                sum += number;
+            }
+        } while (number % 2 != 1);
+
+        System.out.println("Girilen çift ve 4'ün katları olan sayıların toplamı: " + sum);
+    }
+}
