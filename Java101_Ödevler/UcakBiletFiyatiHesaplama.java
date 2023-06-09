b5d9935d417d1f41838ea8c231ee05503615c256
Theo <alpotheo@gmail.com>
2023-06-09 20:24:07 +0300

    Update UcakBiletFiyatiHesaplama.java

--- Java101_Ödevler/UcakBiletFiyatiHesaplama.java
+++ Java101_Ödevler/UcakBiletFiyatiHesaplama.java
@@ -1,12 +1,10 @@
 package Java101_Ödevler;
-
 import java.util.Scanner;
 
 public class UcakBiletFiyatiHesaplama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Kullanıcıdan verileri alıp tanımlıyoruz.
         System.out.print("Mesafeyi (KM) giriniz: ");
         int mesafe = input.nextInt();
 
@@ -16,24 +14,30 @@ public class UcakBiletFiyatiHesaplama {
         System.out.print("Yolculuk tipini giriniz (1: Tek Yön, 2: Gidiş-Dönüş): ");
         int yolculukTipi = input.nextInt();
 
-        // Kullanıcıdan alınan veriler ile fiyatı hesaplayoruz
         if (mesafe > 0 && yas > 0 && (yolculukTipi == 1 || yolculukTipi == 2)) {
             double birimFiyat = 0.10;
-            double toplamFiyat = mesafe * birimFiyat;
-
+            double toplamTutar = mesafe * birimFiyat;
+            
+            double indirimOrani = 0;
             if (yas < 12) {
-                toplamFiyat *= 0.5; // %50 indirim
+                indirimOrani += 0.5; // %50 indirim
             } else if (yas >= 12 && yas <= 24) {
-                toplamFiyat *= 0.9; // %10 indirim
+                indirimOrani += 0.1; // %10 indirim
             } else if (yas >= 65) {
-                toplamFiyat *= 0.7; // %30 indirim
+                indirimOrani += 0.3; // %30 indirim
             }
 
             if (yolculukTipi == 2) {
-                toplamFiyat *= 0.8; // %20 indirim
+                indirimOrani += 0.2; // %20 indirim
             }
 
-            System.out.println("Toplam Tutar: " + toplamFiyat);
+            double indirimliTutar = toplamTutar - (toplamTutar * indirimOrani);
+            double toplamTutarGidisDonus = indirimliTutar;
+            if (yolculukTipi == 2) {
+                toplamTutarGidisDonus *= 2;
+            }
+
+            System.out.println("Toplam Tutar = " + toplamTutarGidisDonus + " TL");
         } else {
             System.out.println("Hatalı Veri Girdiniz!");
         }
