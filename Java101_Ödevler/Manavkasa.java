b9727da645d350f3ef6d7f826e46e0f8dc5b3027
Theo <alpotheo@gmail.com>
2023-06-09 18:02:11 +0300

    Create Manavkasa.java

---
+++ Java101_Ödevler/Manavkasa.java
@@ -0,0 +1,30 @@
+package Java101_Ödevler;
+
+import java.util.Scanner;
+
+public class Manavkasa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double armut = 2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+        double toplam;
+
+        System.out.println("Kaç kilo Armut aldınız: ");
+        double armutKg = input.nextDouble();
+        System.out.println("Kaç kilo Elma aldınız: ");
+        double elmaKg = input.nextDouble();
+        System.out.println("Kaç kilo Domates aldınız: ");
+        double domatesKg = input.nextDouble();
+        System.out.println("Kaç kilo Muz aldınız: ");
+        double muzkg = input.nextDouble();
+        System.out.println("Kaç kilo Patlican aldınız: ");
+        double patlicankg = input.nextDouble();
+
+        toplam = (armut * armutKg) + (elma * elmaKg) + (domates * domatesKg) + (muz * muzkg) + (patlican * patlicankg);
+        System.out.println("Toplam Tutar: "+toplam +" TL");
+        input.close();
+    }
+}
