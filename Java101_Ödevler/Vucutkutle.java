f6db2c954d390f70a94bd98a6519b52361c7d075
Theo <alpotheo@gmail.com>
2023-06-09 18:31:20 +0300

    Yorum satırları eklendi

--- Java101_Ödevler/Vucutkutle.java
+++ Java101_Ödevler/Vucutkutle.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Vucutkutle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Değişkenlerin tanımlanıp kullanıcıdan istenildiği kısım.
         double boy, kilo;
         System.out.print("Lütfen boyunuzu metre cinsinden giriniz: (Örnek: 1,75) ");
         boy = input.nextDouble();
         System.out.print("Lütfen kilonuzu giriniz: (Örnek: 75) ");
         kilo = input.nextDouble();
 
+        // İndeks hesaplamasının yapıldığı kısım.
         double indeks = (kilo / boy) /boy;
         System.out.println("Vücut kitle İndeksiniz: "+indeks);
         input.close();
