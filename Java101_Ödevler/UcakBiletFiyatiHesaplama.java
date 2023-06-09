495ae6cd447aa760b196fe84f946ea28d1b8ec45
Theo <alpotheo@gmail.com>
2023-06-09 20:27:05 +0300

    Update UcakBiletFiyatiHesaplama.java

--- Java101_Ödevler/UcakBiletFiyatiHesaplama.java
+++ Java101_Ödevler/UcakBiletFiyatiHesaplama.java
@@ -15,10 +15,12 @@ public class UcakBiletFiyatiHesaplama {
         System.out.print("Yolculuk tipini giriniz (1: Tek Yön, 2: Gidiş-Dönüş): ");
         int yolculukTipi = input.nextInt();
 
+        // Tutarın hesaplandığı kısım.
         if (mesafe > 0 && yas > 0 && (yolculukTipi == 1 || yolculukTipi == 2)) {
             double birimFiyat = 0.10;
             double toplamTutar = mesafe * birimFiyat;
-            
+        
+            // İndirim oranının uygulandığı kısım.
             double indirimOrani = 0;
             if (yas < 12) {
                 indirimOrani += 0.5; // %50 indirim
@@ -31,7 +33,7 @@ public class UcakBiletFiyatiHesaplama {
             if (yolculukTipi == 2) {
                 indirimOrani += 0.2; // %20 indirim
             }
-
+            // İndirim tutarının hesaplandığı kısım.
             double indirimliTutar = toplamTutar - (toplamTutar * indirimOrani);
             double toplamTutarGidisDonus = indirimliTutar;
             if (yolculukTipi == 2) {
