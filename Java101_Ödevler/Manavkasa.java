f77582eb2adbc9c99a24af3e9624f26420ea8788
Theo <alpotheo@gmail.com>
2023-06-09 18:32:46 +0300

    Yorum satırları eklendi

--- Java101_Ödevler/Manavkasa.java
+++ Java101_Ödevler/Manavkasa.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Manavkasa {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Meyveler kg fiyatlarına göre tanımlandı.
         double armut = 2.14;
         double elma = 3.67;
         double domates = 1.11;
@@ -12,6 +14,7 @@ public class Manavkasa {
         double patlican = 5.00;
         double toplam;
 
+        // Kullanıcıdan kaç kg alındığına dair bilgi alınıp tanımlanıyor.
         System.out.println("Kaç kilo Armut aldınız: ");
         double armutKg = input.nextDouble();
         System.out.println("Kaç kilo Elma aldınız: ");
@@ -22,7 +25,8 @@ public class Manavkasa {
         double muzkg = input.nextDouble();
         System.out.println("Kaç kilo Patlican aldınız: ");
         double patlicankg = input.nextDouble();
-
+        
+        // Toplam hesabı yapılıp ekrana yazdırılıyor.
         toplam = (armut * armutKg) + (elma * elmaKg) + (domates * domatesKg) + (muz * muzkg) + (patlican * patlicankg);
         System.out.println("Toplam Tutar: "+toplam +" TL");
         input.close();
