70fed54e3a79834a2c92e9c7d89088d475832e7b
Theo <alpotheo@gmail.com>
2023-06-09 17:53:02 +0300

    Create Vucutkutle.java

---
+++ Java101_Ödevler/Vucutkutle.java
@@ -0,0 +1,18 @@
+package Java101_Ödevler;
+
+import java.util.Scanner;
+
+public class Vucutkutle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double boy, kilo;
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: (Örnek: 1,75) ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: (Örnek: 75) ");
+        kilo = input.nextDouble();
+
+        double indeks = (kilo / boy) /boy;
+        System.out.println("Vücut kitle İndeksiniz: "+indeks);
+        input.close();
+    }
+}
