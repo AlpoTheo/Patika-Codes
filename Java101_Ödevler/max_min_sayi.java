4c5f8f690c1c590a3d86fb7e296b05f20c065b5f
Theo <alpotheo@gmail.com>
2023-06-11 00:11:22 +0300

    Update max_min_sayi.java

--- Java101_Ödevler/max_min_sayi.java
+++ Java101_Ödevler/max_min_sayi.java
@@ -4,12 +4,15 @@ public class max_min_sayi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // Kullanıcıdan kaç sayi bilgisi alınır.
         System.out.print("Kaç tane sayı gireceksiniz: ");
         int n = input.nextInt();
 
+        // max ve min değerleri en küçük ve en büyük değerler olarak alınıp işlemde hata çıkmaması için tanımlanır.
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
+        // Kaçıncı sayı değeri döndürülür ve if kısımlarında max ve min sayı hesaplanır.
         for (int i = 1; i <= n; i++) {
             System.out.print(i + ". Sayıyı giriniz: ");
             int sayi = input.nextInt();
@@ -22,7 +25,7 @@ public class max_min_sayi {
                 min = sayi;
             }
         }
-
+        // En büyük ve en küçük sayı ekrana yazdırılır.
         System.out.println("En büyük sayı: " + max);
         System.out.println("En küçük sayı: " + min);
     input.close();
