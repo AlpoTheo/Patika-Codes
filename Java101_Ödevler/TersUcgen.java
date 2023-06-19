ed12df94f0cd807ed7a2882d4acb0d0bdc8e8a52
Theo <alpotheo@gmail.com>
2023-06-19 13:35:48 +0300

    Create TersUcgen.java

---
+++ Java101_Ödevler/TersUcgen.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Basamak sayısı kullanıcıdan alınıyor.
+        System.out.print("Basamak Sayısı: ");
+        int Digits = scanner.nextInt();
+
+        int star = Digits * 2 - 1;
+        int space = 0;
+
+        //  Alınan basamak sayısına göre for döngüleriyle "*" konularak ters üçgen çizdiriliyor.
+        for (int i = 1; i <= Digits; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= star; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            star -= 2;
+            space++;
+        }
+        // Scanner kapatılıyor.
+        scanner.close();
+    }
+}
